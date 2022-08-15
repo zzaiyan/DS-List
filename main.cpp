b2ce42e755a4a997770a75f61436b1cdab8268eb
zzaiyan@outlook.com <zzaiyan@outlook.com>
2022-08-16 01:59:16 +0800

    optimized

--- main.cpp
+++ main.cpp
@@ -2,22 +2,6 @@
 #include <iostream>
 using namespace std;
 
-int getRank(List<int> &L, ListNode<int> *p) {
-  int n = 0;
-  while (p != L.begin()) {
-    p = p->pred;
-    n++;
-  }
-  return n;
-}
-
-ListNode<int> *getNode(List<int> &L, int n) {
-  auto p = L.begin();
-  while (n--)
-    p = p->succ;
-  return p;
-}
-
 int main() {
   List<int> L;
   int n;
@@ -25,17 +9,6 @@ int main() {
   for (int i = 0; i < n; i++) {
     L.pushBack(rand() % 100 + 1);
   }
-  // while (n--) {
-  //   // string in;
-  //   // cin >> in;
-  //   int x, y;
-  //   cin >> x >> y;
-  //   L.insertAfter(L.begin()->pred, x);
-  //   L.insertBefore(L.end(), y);
-  //   for (auto i = L.first(); i != L.end(); i = i->succ)
-  //     cout << i->date << ' ';
-  //   cout << endl;
-  // }
 
   for (auto i = L.first(); i != L.end(); i = i->succ)
     cout << i->date << ' ';
